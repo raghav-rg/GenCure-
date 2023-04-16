8db552ff3eaad8769b5215fca2c8a0941cd59c19
Raghav Gupta <raghavgupta14.rg@gmail.com>
2023-04-16 09:21:50 +0530

    Update [id].js

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -4,10 +4,10 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useContext } from 'react';
 import { toast } from 'react-toastify';
-import Layout from '../../components/Layout';
-import Product from '../../models/Product';
-import db from '../../utils/db';
-import { Store } from '../../utils/Store';
+import Layout from '/components/Layout.js';
+import Product from '/models/Product';
+import db from '/utils/db';
+import { Store } from '/utils/Store';
 
 export default function ProductScreen(props) {
   const { product } = props;
